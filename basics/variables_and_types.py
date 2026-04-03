13a93171b86b7def10c6134b5ce349115d3b293e
kevin <kevin@hinshawfam.com>
2026-04-02 20:33:36 -0500

    Add variables and types example

---
+++ basics/variables_and_types.py
@@ -0,0 +1,28 @@
+# variables_and_types.py
+
+# Strings
+name = "Kevin"
+
+# Integers
+age = 30
+
+# Floats
+height = 5.11
+
+# Booleans
+is_learning = True
+favorite_language = "C#"  # This variable is currently not assigned a value
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Learning Python:", is_learning)
+print(f"My name is {name}, I am {age} years old, and I am {height} feet tall.")
+print(f"My favorite programming language is {favorite_language}.")
+
+# Type checking
+print("\n--- Types ---")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
