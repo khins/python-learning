527e6b0d38d6e95b606e03c6b5bc72a42c761002
khins <kevin@hinshawfam.com>
2026-08-30 16:46:52 -0500

    add string cleanup example

---
+++ basics/string_cleanup.py
@@ -0,0 +1,19 @@
+'''
+string_cleanup.py
+# Define a fancy_cleanup function that accepts a single string argument
+# The function should clean up the whitespace on both sides of the
+# argument. It should also replace every occurrence of the letter "g" with the
+# letter "z" and every occurrence of a space with an exclamation point (!).
+#
+# fancy_cleanup("       geronimo crikey  ")   => "zeronimo!crikey"
+# fancy_cleanup("       nonsense  ")          => "nonsense"
+# fancy_cleanup("grade")                      => "zrade"
+
+'''
+def fancy_cleanup(text):
+    return text.strip().replace("g","z").replace(" ","!")
+  
+
+print(fancy_cleanup("       geronimo crikey  "))   #=> "zeronimo!crikey")
+print(fancy_cleanup("       nonsense  "))         # => "nonsense")
+print(fancy_cleanup("grade"))                      # => "zrade")
