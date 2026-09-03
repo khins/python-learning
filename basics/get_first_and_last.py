16a22a3fcc4f11aef4bfb20fb4548fe8dcf6fceb
khins <kevin@hinshawfam.com>
2026-09-03 15:37:33 -0500

    add get first and last from list

--- basics/get_first_and_last.py
+++ basics/get_first_and_last.py
@@ -3,6 +3,11 @@ get_first_and_last.py
 Lists Exercise 1: Create and Access a List
 Create a function named get_first_and_last that accepts a list and returns a tuple containing:
 (first item, last item)
+Requirements:
+Use list indexing.
+Do not use a loop.
+Do not modify the original list.
+Use a negative index to access the final item.
 '''
 def get_first_and_last(mylist):
     outlist = []
