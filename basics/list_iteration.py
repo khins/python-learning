e35947dcb60532e55740748735f22777bb9b5973
khins <kevin@hinshawfam.com>
2026-09-04 20:01:49 -0500

    updates list iteration

--- basics/list_iteration.py
+++ basics/list_iteration.py
@@ -23,9 +23,16 @@ print(smallest_number([-3, -2, -1])) # => -3
 # all list elements whose length is greater than 2 characters.
 #
 # EXAMPLES
-# concatenate(["abc", "def", "ghi"])          => "abcdefghi"
-# concatenate(["abc", "de", "fghi", "ic"])    => "abcfghi"
-# concatenate(["ab", "cd", "ef", "gh"])       => ""
+def concatenate(values):
+    string = ""
+    for word in values:
+        if len(word) > 2:
+            string += word
+    return string
+
+print(concatenate(["abc", "def", "ghi"])) #          => "abcdefghi"
+print(concatenate(["abc", "de", "fghi", "ic"])) #    => "abcfghi"
+print(concatenate(["ab", "cd", "ef", "gh"])) #       => ""
 
 
 # Define a super_sum function that accepts a list of strings.
@@ -33,3 +40,29 @@ print(smallest_number([-3, -2, -1])) # => -3
 #
 # Not every word is guaranteed to have an "s".
 # Don't use "sum" as a variable name as it's a built-in keyword.
+
+# Define a super_sum function that accepts a list of strings.
+# The function should sum the index positions of
+# the first occurrence of the letter "s" in each word.
+#
+# Not every word is guaranteed to have an "s".
+# Don't use "sum" as a variable name as it's a built-in keyword.
+#
+# EXAMPLES
+def super_sum(values):
+    total = 0
+    if len(values) == 0:
+        return 0
+    for word in values:
+        if "s" in word:
+            index = word.index("s")
+            total += index
+    return total
+
+
+
+print(super_sum([])) #                                     => 0
+print(super_sum(["mustache"])) #                            => 2
+print(super_sum(["mustache", "greatest"])) #                => 8
+print(super_sum(["mustache", "pessimist"])) #               => 4
+print(super_sum(["mustache", "greatest", "almost"])) #      => 12
